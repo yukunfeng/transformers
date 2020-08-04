203b5747e0c3220cfea2abf140d5bf42c758635a
yukun feng <yukunfg@gmail.com>
2020-08-04 17:36:02 +0900

    fixed for sklearn f1 metric for micro score

--- src/transformers/data/metrics/__init__.py
+++ src/transformers/data/metrics/__init__.py
@@ -34,7 +34,8 @@ if _has_sklearn:
 
     def acc_and_f1(preds, labels):
         acc = simple_accuracy(preds, labels)
-        f1 = f1_score(y_true=labels, y_pred=preds)
+        #  f1 = f1_score(y_true=labels, y_pred=preds)
+        f1 = f1_score(y_true=labels, y_pred=preds, average='micro')
         return {
             "acc": acc,
             "f1": f1,
