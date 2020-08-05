3a61b5c344dd0aaecf5e2de86de06efa101817f0
yukunfeng <yukunfg@gmail.com>
2020-08-05 19:06:59 +0900

    lower fewrel

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -424,6 +424,7 @@ class FewrelProcessor(DataProcessor):
           h_name = sent[h[1]:h[2]]
           t_name = sent[t[1]:t[2]]
           new_sent = sent[:h[1]] + "# "+h_name+" #" + sent[h[2]:t[1]] + "$ "+t_name+" $" + sent[t[2]:]
+          new_sent = new_sent.lower()
           lines.append([label, new_sent])
         return lines
 
