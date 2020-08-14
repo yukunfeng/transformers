06266c9d45553eb0f951336fde6a56632a5bdf8d
yukunfeng <yukunfg@gmail.com>
2020-08-14 10:41:33 +0900

    up sh

--- run_fewrel.sh
+++ run_fewrel.sh
@@ -4,10 +4,10 @@ export GLUE_DIR=/home/lr/yukun/kg-bert/ERNIE/data/
 task_name="fewrel"
 data_dir="$GLUE_DIR/$task_name"
 output_dir="${task_name}_output/"
+rm -rf $output_dir
 
-  # --model_name_or_path 'roberta-base' \
 python ./examples/text-classification/run_glue.py \
-  --model_name_or_path 'bert-base-uncased' \
+  --model_name_or_path 'roberta-base' \
   --task_name $task_name \
   --do_train \
   --do_eval \
@@ -15,7 +15,7 @@ python ./examples/text-classification/run_glue.py \
   --data_dir $data_dir \
   --max_seq_length 256 \
   --per_device_train_batch_size 16 \
-  --learning_rate 2e-5 \
+  --learning_rate 5e-5 \
   --num_train_epochs 10 \
   --save_steps 2000 \
   --save_total_limit 1 \
@@ -26,4 +26,4 @@ python ./examples/text-classification/run_glue.py \
   --output_dir $output_dir
 
 python score.py -gold_file "$data_dir/test.json" -pred_file "$output_dir/test_results_${task_name}.txt"
-python ~/env_config/sending_emails.py -c "succ: $? fewrel"
+python ~/env_config/sending_emails.py -c "succ: $? fewrel:trans"
--- run_tacred.sh
+++ run_tacred.sh
@@ -1,24 +1,28 @@
 set -x
 export GLUE_DIR=/home/lr/yukun/kg-bert/ERNIE/data/
 
-task_name="tacred"
-data_dir="$GLUE_DIR/$task_name"
-output_dir="${task_name}_output/"
+task_name="fewrel"
+data_dir="$GLUE_DIR/tacred"
+output_dir="tacred_output/"
+rm -rf $output_dir
 
-  # --model_name_or_path 'roberta-base' \
 python ./examples/text-classification/run_glue.py \
-  --model_name_or_path 'bert-base-uncased' \
-  --task_name 'fewrel' \
+  --model_name_or_path 'roberta-base' \
+  --task_name $task_name \
   --do_train \
   --do_eval \
   --do_predict \
   --data_dir $data_dir \
-  --max_seq_length 128 \
-  --per_device_train_batch_size 32 \
+  --max_seq_length 256 \
+  --per_device_train_batch_size 16 \
   --learning_rate 2e-5 \
-  --num_train_epochs 10 \
+  --num_train_epochs 5 \
   --save_steps 2000 \
   --save_total_limit 1 \
+  --logging_steps 1000 \
+  --evaluate_during_training \
+  --eval_steps 1000 \
+  --warmup_steps 500 \
   --output_dir $output_dir
 
 python score.py -gold_file "$data_dir/test.json" -pred_file "$output_dir/test_results_${task_name}.txt"
