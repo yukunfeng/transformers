79a316485acdfb83eda01a69db5e2bb812cdba61
yukun feng <yukunfg@gmail.com>
2020-08-14 21:06:34 +0900

    up fewrel sh

--- run_fewrel.sh
+++ run_fewrel.sh
@@ -1,29 +1,48 @@
 set -x
-export GLUE_DIR=/home/lr/yukun/kg-bert/ERNIE/data/
+export GLUE_DIR=./glue_data/
 
 task_name="fewrel"
 data_dir="$GLUE_DIR/$task_name"
 output_dir="${task_name}_output/"
-rm -rf $output_dir
+pred_file="$output_dir/test_results_${task_name}.txt"
 
-python ./examples/text-classification/run_glue.py \
-  --model_name_or_path 'roberta-base' \
-  --task_name $task_name \
-  --do_train \
-  --do_eval \
-  --do_predict \
-  --data_dir $data_dir \
-  --max_seq_length 256 \
-  --per_device_train_batch_size 16 \
-  --learning_rate 5e-5 \
-  --num_train_epochs 10 \
-  --save_steps 2000 \
-  --save_total_limit 1 \
-  --logging_steps 1000 \
-  --evaluate_during_training \
-  --eval_steps 1000 \
-  --warmup_steps 500 \
-  --output_dir $output_dir
+batch_sizes=(32 16)
+lrs=(1e-5 2e-5 5e-6 1e-6)
+warmups=(0 200 500 800 1000)
 
-python score.py -gold_file "$data_dir/test.json" -pred_file "$output_dir/test_results_${task_name}.txt"
-python ~/env_config/sending_emails.py -c "succ: $? fewrel:trans"
+for batch_size in "${batch_sizes[@]}"
+do
+  for lr in "${lrs[@]}"
+  do
+    for warmup in "${warmups[@]}"
+    do
+        rm -rf $output_dir
+        python ./examples/text-classification/run_glue.py \
+          --model_name_or_path 'roberta-large' \
+          --task_name $task_name \
+          --do_train \
+          --do_eval \
+          --do_predict \
+          --data_dir $data_dir \
+          --max_seq_length 256 \
+          --per_device_train_batch_size $batch_size \
+          --learning_rate $lr \
+          --num_train_epochs 10 \
+          --save_steps 4000 \
+          --save_total_limit 1 \
+          --logging_steps 1000 \
+          --evaluate_during_training \
+          --eval_steps 4000 \
+          --warmup_steps $warmup \
+          --output_dir $output_dir
+
+        echo "batch_size: $batch_size lr:$lr warmup:$warmup"
+        python score.py -gold_file "$data_dir/test.json" -pred_file $pred_file
+    done
+    python ~/env_config/sending_emails.py -c "succ: $? fewrel. Warmup finished"
+    exit 0
+  done
+done
+
+
+
