e549b689585c4cc4472be73c7b5d5180281c4210
yukunfeng <yukunfg@gmail.com>
2020-12-14 18:48:34 +0900

    add some minor stuff

--- run.sh
+++ run.sh
@@ -3,22 +3,22 @@ export GLUE_DIR=./glue_data/
 
 # python download_glue_data.py --data_dir glue_data --tasks all
 
-# task_name="sent_classify"
+task_name="sent_classify"
   # --do_eval \
   # --do_train \
   # --model_name_or_path 'roberta-base' \
-# python ./examples/text-classification/run_glue.py \
-  # --task_name $task_name \
-  # --model_name_or_path './sent_classify_framebyf_output/' \
-  # --do_predict \
-  # --data_dir $GLUE_DIR/frame_classify_byframe \
-  # --max_seq_length 128 \
-  # --per_device_train_batch_size 32 \
-  # --learning_rate 2e-5 \
-  # --num_train_epochs 10.0 \
-  # --save_steps 2000 \
-  # --save_total_limit 1 \
-  # --output_dir ./${task_name}_framebyf_output/
+python ./examples/text-classification/run_glue.py \
+  --task_name $task_name \
+  --model_name_or_path './sent_classify_framebyf_output/' \
+  --do_predict \
+  --data_dir $GLUE_DIR/frame_classify_byframe \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 10.0 \
+  --save_steps 2000 \
+  --save_total_limit 1 \
+  --output_dir ./${task_name}_framebyf_output/
 
 # python score.py -gold_file glue_data/frame_classify_byframe/test.tsv -pred_file  ./${task_name}_framebyf_output/test_results_sent_classify.txt
 # python ~/env_config/sending_emails.py -c "succ: $? zp classify frame byframe"
